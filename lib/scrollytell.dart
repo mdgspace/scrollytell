ad8f4eb756dca05ca5b88d7b930ef5d890ced6d0
am15h <amishgarg123@gmail.com>
2019-12-06 15:46:26 +0530

    filling _panelheights array

--- lib/scrollytell.dart
+++ lib/scrollytell.dart
@@ -1,5 +1,7 @@
 library scrollytell;
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 // Using ScrollController instead of ScrollNotification - https://api.flutter.dev/flutter/widgets/ScrollNotification-class.html
@@ -28,23 +30,24 @@ class _ScrollyWidgetState extends State<ScrollyWidget> {
   // panel height, Using Map to maintain proper mapping(panelNumber, panelHeight) at all the time
   Map<int, double> _panelHeights;
 
-  List<PanelWidget> _statefulPanels;
-
+  int _heightFillIndex;
 
   final keys = new List.generate(20, (_) => new GlobalKey<_PanelWidgetState>());
 
   // Scroll Listener
   // Todo: Temporarily manipulating the overlayWidget in the scrollListener, add the wrapper later
 
-  _scrollListener(){
-    print("Scroll Offset ${_scrollController.offset}");
-    _getHeight();
-
+  _scrollListener() {
+//        print("Scroll Offset ${_scrollController.offset}");
+    //TODO: change to variable height implementation using panelPrefixHeights
+    if (_heightFillIndex != -1 &&
+        _scrollController.offset >= _panelHeights[0] * (_heightFillIndex - 1)) {
+      _getHeight();
+    }
   }
 
   @override
   void initState() {
-
     // Scroll Controller and Listener
     _scrollController = ScrollController();
     _scrollController.addListener(_scrollListener);
@@ -58,21 +61,17 @@ class _ScrollyWidgetState extends State<ScrollyWidget> {
     //Call after render
     WidgetsBinding.instance.addPostFrameCallback((_) => _getHeight());
 
-
     // TODO:: Temporary(to be changed later): Initializing overlayWidget in initState()
     overLayWidget = Opacity(
       opacity: 0.7,
       child:
-      Container(padding: EdgeInsets.all(16), child: Text('OverlayWidget')),
+          Container(padding: EdgeInsets.all(16), child: Text('OverlayWidget')),
     );
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-
-
-
     // The stack paints its children in order with the first child being at the bottom.
     return Stack(
       children: <Widget>[
@@ -80,12 +79,15 @@ class _ScrollyWidgetState extends State<ScrollyWidget> {
           controller: _scrollController,
           //TODO: Provide flexibility to directly input sliverList
           slivers: <Widget>[
-            SliverList(delegate: SliverChildBuilderDelegate(
-                    (BuildContext context, int index){
-                  return PanelWidget(key: keys[index], rawPanel: widget.panels[index],);
-                },
-                childCount: 20
-            ),)
+            SliverList(
+              delegate:
+              SliverChildBuilderDelegate((BuildContext context, int index) {
+                return PanelWidget(
+                  key: keys[index],
+                  rawPanel: widget.panels[index],
+                );
+              }, childCount: 20),
+            )
           ],
         ),
         overLayWidget
@@ -94,20 +96,57 @@ class _ScrollyWidgetState extends State<ScrollyWidget> {
   }
 
   void _getHeight() {
-    final List<State> states = List.generate(20, (index) => keys[index].currentState);
-    final List<BuildContext> contexts = List.generate(20, (index) => keys[index].currentContext);
+    final List<State> states =
+    List.generate(20, (index) => keys[index].currentState);
+    final List<BuildContext> contexts =
+    List.generate(20, (index) => keys[index].currentContext);
 
-    final List<RenderBox> boxes = List.generate(20, (index) => states[index]?.context?.findRenderObject());
+    final List<RenderBox> boxes = List.generate(
+        20, (index) => states[index]?.context?.findRenderObject());
 
-    boxes.asMap().forEach((index, box) => print("Box $index Height : ${box?.size?.height}"));
+    boxes.asMap().forEach(
+            (index, box) => print("Box $index Height : ${box?.size?.height}"));
+
+
+    setState(() {
+      _panelHeights = List.generate(20, (index) {
+        if (_panelHeights[index] == null) {
+          return boxes[index]?.size?.height;
+        } else {
+          return _panelHeights[index];
+        }
+      }).asMap();
+
+    });
+
+    final nullIndex = getNullIndex();
+    print("Null Index $nullIndex");
+
+    setState(() {
+      _heightFillIndex = nullIndex;
+    });
+
+    print(_panelHeights);
+    print(_heightFillIndex);
 
   }
 
+  getNullIndex() {
+
+    for(var i = 0; i < _panelHeights.length; i++){
+      if(_panelHeights[i] == null){
+        return i;
+      }
+    }
+
+    return -1;
+  }
 }
 
-class PanelWidget extends StatefulWidget{
-
-  PanelWidget({Key key, Widget rawPanel}) : rawPanel = rawPanel, super(key: key);
+class PanelWidget extends StatefulWidget {
+  PanelWidget({Key key, Widget rawPanel})
+      : rawPanel = rawPanel,
+        super(key: key);
 
   final Widget rawPanel;
 
