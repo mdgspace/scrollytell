f68b3d99139549ca04d06fdbe8bdbec9cff07833
am15h <amishgarg123@gmail.com>
2019-12-06 14:08:00 +0530

    initial commit

---
+++ lib/scrollytell.dart
@@ -0,0 +1,5 @@
+library scrollytell;
+
+class ScrollyTell {
+
+}
---
+++ test/scrollytell_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:scrollytell/scrollytell.dart';
+
+void main() {
+
+}
